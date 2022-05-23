3e02c18cf3aa15e54369fd389f8b2310d14aaf64
Keisuke Kan <9renpoto@gmail.com>
2022-05-23 11:43:50 +0900

    chore: build

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+export * from './pubsub.constants';
+export * from './pubsub.inject';
+export * from './pubsub.interface';
+export * from './pubsub.module';
---
+++ lib/pubsub.inject.d.ts
@@ -0,0 +1,1 @@
+export declare function InjectPubsub(): (target: object, key: string | symbol, index?: number) => void;
